c45854a3f3046f617134becda34869217e7d2af6
MarcManiez <marc.maniez@gmail.com>
2021-12-04 15:49:05 -0800

    Resolve conflict

--- all-the-addresses-client/pages/_app.tsx
+++ all-the-addresses-client/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+
+const getLibrary = (provider: any) => new Web3(provider);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
