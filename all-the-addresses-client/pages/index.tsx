11e22715f566839f2c4e192303ddcb2b76e70137
MarcManiez <marc.maniez@gmail.com>
2021-12-04 16:30:07 -0800

    Revert me

--- all-the-addresses-client/pages/index.tsx
+++ all-the-addresses-client/pages/index.tsx
@@ -11,8 +11,10 @@ import { Contract } from "web3-eth-contract";
 
 // Assumes a deployed contract address. #HACKATHON
 const contractAddress = "0x4de47BA468ec1F2a4a1E1f507512178ECd86803b";
-import contractAbi from "../../interfaces/abi/PhysicalAddressValidation.abi";
+import contractAbi from "../../build/contracts/PhysicalAddressValidation.json";
 // We will somehow need to get the abi
+
+const postOfficePubicKey = "asdf";
 
 const QrReader = dynamic(() => import("react-qr-reader"), {
   ssr: false,
@@ -23,6 +25,8 @@ interface QrCode {
   nonce: string;
   physicalAddressHash: string;
   addressMetadata: string;
+  notsecurenonce: string;
+  proofOfAddress: string;
 }
 
 const Home: NextPage = () => {
@@ -30,8 +34,9 @@ const Home: NextPage = () => {
     useWeb3React<Web3>();
   const connect = async () => activate(injected);
   const disconnect = async () => deactivate();
-  const [qrCode, setQrCode] = useState<QrCode>();
   const [contract, setContract] = useState<Contract>();
+  const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
+  const [isScanned, setIsScanned] = useState<boolean>(false);
 
   useEffect(() => {
     if (!library) return;
@@ -44,6 +49,23 @@ const Home: NextPage = () => {
       setContract(contract);
     })();
   }, [library]);
+  console.log("contract", contract);
+  const mapAddress = async (qrCode: QrCode) => {
+    const { physicalAddressHash, notsecurenonce, proofOfAddress } = qrCode;
+    try {
+      await contract?.methods
+        .registerAddress(physicalAddressHash, notsecurenonce, proofOfAddress)
+        .send({ from: account })
+        .on("receipt", () => setIsConfirmed(true));
+    } catch (error) {
+      console.log("error", error);
+      console.log("error", (error as any).data);
+      const message = JSON.parse(
+        (error as any).message.substring(15).trim()
+      ).message;
+      console.log("message", message);
+    }
+  };
 
   // Scan a QR code
   // Input the QR code data into a form (optional?)
@@ -56,17 +78,24 @@ const Home: NextPage = () => {
         <button onClick={connect}>Connect to Metamask</button>
       )}
       <p>{active ? `Connected with ${account}` : "not connected"}</p>
-      <QrReader
-        delay={300}
-        onError={(error: any) => console.error(error)}
-        onScan={(data: any) => {
-          if (!data) return;
-          // The data will be signed with a private key
-          // Verify this using the public key
-          setQrCode(JSON.parse(data) as QrCode);
-        }}
-        style={{ width: "100%" }}
-      />
+      {!isScanned && (
+        <QrReader
+          delay={300}
+          onError={(error: any) => console.error(error)}
+          onScan={async (data: any) => {
+            if (!data || isScanned) return;
+            setIsScanned(true);
+            // The data will be signed with a private key
+            // Verify this using the public key
+            // setQrCode(JSON.parse(data) as QrCode);
+            const qrCode = JSON.parse(data) as QrCode;
+            await mapAddress(qrCode);
+          }}
+          style={{ width: "20%" }}
+        />
+      )}
+
+      {isConfirmed && <div>OMG congrats, we did it team!</div>}
     </div>
   );
 };
